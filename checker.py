656ad452900dcf91e4c72f6990483045fcdddc08
Nick Triantafillou <nicolast@gmail.com>
2014-06-25 15:59:35 +1000

    checker emailing properly and working with modules

--- checker.py
+++ checker.py
@@ -2,15 +2,17 @@ import hashlib
 import pickle
 import imp
 import subprocess
+import sys
 
 # again, read this from an ini
-sites = ['uow']
+sites = ['uow', 'atlassian']
 
 # run sites.py for each site
-import uow
-uow.main()
-import atlassian
-atlassian.main()
+for s in sites:
+	if sys.modules.has_key(s) and isinstance(sys.modules[s], types.ModuleType):
+		continue
+	sys.modules[s] = __import__(s, globals(), locals(), [''])
+	sys.modules[s].main()
 
 # checksum calculator for each file to see if it has changed
 def checksum(f):
@@ -19,12 +21,12 @@ def checksum(f):
     return md5.hexdigest()
 
 def sendemail(msg):
-    process = subprocess.Popen(['mail', '-s', "New Jobs!", "nick@triso.me"],
+    process = subprocess.Popen(['mail', '-s', "New Jobs!", "nicolast@gmail.com"],
                                stdin=subprocess.PIPE)
     process.communicate(msg)
 
 	
-def whatsdifferent(file1, file2):
+def whatsdifferent(file1, file2, emailmsg):
 	jobsold = pickle.load(open(file1))
 	jobsnew = pickle.load(open(file2))
 	new = {}
@@ -45,20 +47,30 @@ def whatsdifferent(file1, file2):
 			new[key] = jobsnew[key]
 
 	if (len(new) > 0):
-		emailmsg = "New jobs:\n"
+		if not emailmsg:
+			emailmsg = "New jobs:\n\n"
 		for job in new:
 			emailmsg += "%s - %s\n" % (job, new[job])
-		sendemail(emailmsg)
+		# then write the jobsnew.pickle to the jobsold.pickle filename
+		pickle.dump(jobsnew, open(file1, "wb"))
+
+	return emailmsg
 		
 
 # main
 def main():
+	emailmsg = ""
 	for s in sites:
 		file1 = "tmp/" + s + ".pickle"
 		file2 = "tmp/" + s + ".pickle.new"
 		if checksum(file1) != checksum(file2):
 			# find out why they're different
-			whatsdifferent(file1, file2) 
+			emailmsg = whatsdifferent(file1, file2, emailmsg) 
+
+	if (emailmsg):
+		sendemail(emailmsg)
+	else:
+		sendemail("no new jobs")
 
 if __name__ == "__main__":
     main()
